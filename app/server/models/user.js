8fc66e97d73fd23e67a6361c2b89e5a06d7c70eb
lawrence <lawrence.olaiya@andela.com>
2016-01-24 20:27:21 -0800

    Update user model to enable google and facebook auth Remove unique field restriction on some fields Update user model to accomodate upload of profile image Correct indentation

--- app/server/models/user.js
+++ app/server/models/user.js
@@ -1,58 +1,76 @@
 var mongoose = require('mongoose'),
-    mongoosePaginate = require('mongoose-paginate'),
-    Schema = mongoose.Schema,
-    ObjectId = Schema.Types.ObjectId;
+  mongoosePaginate = require('mongoose-paginate'),
+  Schema = mongoose.Schema,
+  ObjectId = Schema.Types.ObjectId;
 
 var UserSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
+  username: {
+    type: String
+  },
+  name: {
+    first: {
+      type: String
     },
-    name: {
-        first: {
-            type: String,
-            required: true
-        },
-        last: {
-            type: String,
-            required: true
-        },
+    last: {
+      type: String
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  google: {
+    id: {
+      type: String
     },
-    hashedPass: {
-        type: String,
-        required: true
+    token: {
+      type: String
+    }
+  },
+  facebook: {
+    id: {
+      type: String
     },
-    saltPass: {
-        type: String,
-        required: true
-    },
-    createdAt: {
-        required: true,
-        type: Date,
-        default: Date.now
-    },
-    updatedAt: {
-        required: true,
-        type: Date,
-        default: Date.now
-    },
-    role: [{
-        required: true,
-        type: ObjectId,
-        ref: 'Role'
-    }]
+    token: {
+      type: String
+    }
+  },
+  hashedPass: {
+    type: String
+  },
+  saltPass: {
+    type: String
+  },
+  img_url: {
+    type: String,
+    default: 'http://res.cloudinary.com/dms/image/upload/c_scale,h_275,q_98,' +
+      'r_30/v1453209823/default_avatar_fnm9wb.gif'
+  },
+  public_img_id: {
+    type: String
+  },
+  createdAt: {
+    required: true,
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    required: true,
+    type: Date,
+    default: Date.now
+  },
+  role: [{
+    required: true,
+    type: ObjectId,
+    ref: 'Role'
+  }]
 });
 
 // load the mongoose paginate plugin into the User Schema.
 UserSchema.plugin(mongoosePaginate);
 
 module.exports = {
-    model: mongoose.model('User', UserSchema),
-    schema: UserSchema
+  model: mongoose.model('User', UserSchema),
+  schema: UserSchema
 };
