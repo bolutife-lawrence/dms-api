78623105ba7ade9415b28027457bb7b435b55473
lawrence <lawrence.olaiya@andela.com>
2016-02-18 02:00:09 -0800

    Generate  token from authenticated user Fix duplicate creation of users after authentication Retrieve user after authentication

--- app/config/passport.js
+++ app/config/passport.js
@@ -1,14 +1,25 @@
 var FacebookStrategy = require('passport-facebook').Strategy,
   GoogleStrategy = require('passport-google-oauth').OAuth2Strategy,
+  jwt = require('jsonwebtoken'),
   // load up the models
   models = require('../server/models'),
   // load the auth variables
   configAuth = require('./auth'),
 
+  signUser = (user) => {
+    var options = {
+        expiresIn: '24h' // expires in 24 hours from creation.
+      },
+      token = jwt.sign(user, process.env.WEB_TOKEN_SECRET, options);
+    var modifiedUser = Object.assign({}, user);
+    modifiedUser._doc.token = token;
+    return modifiedUser._doc;
+  },
+
   passportConfig = (passport) => {
     // used to serialize the user for the session
     passport.serializeUser((user, done) => {
-      done(null, user.id);
+      done(null, user);
     });
 
     // used to deserialize the user
@@ -32,46 +43,36 @@ var FacebookStrategy = require('passport-facebook').Strategy,
         // check if the user is already logged in
         if (!req.user) {
           models.User.findOne({
-            'facebook.id': profile.id
-          }, (err, user) => {
-            if (err) {
-              return done(err);
-            }
-            if (user) {
-              // if there is a user id already but no token
-              //(user was linked at one point and then removed)
-              if (!user.facebook.token) {
-                user.facebook.token = token;
-                user.name.last = profile._json.last_name;
-                user.name.first = profile._json.first_name;
-                user.email = profile._json.email;
-                user.img_url = profile._json.picture.data.url;
-                user.save((err) => {
+              'facebook.id': profile.id
+            }, (err, user) => {
+              if (err) {
+                return done(err);
+              }
+              if (user) {
+                user = signUser(user);
+                return done(null, user); // user found, return that user
+              } else {
+                // if there is no user, create them
+                var newUser = new models.User();
+                newUser.facebook.id = profile.id;
+                newUser.facebook.token = token;
+                newUser.name.last = profile._json.last_name;
+                newUser.name.first = profile._json.first_name;
+                newUser.email = profile._json.email;
+                newUser.img_url = profile._json.picture.data.url;
+                newUser.save((err) => {
                   if (err) {
                     return done(err);
                   }
-                  return done(null, user);
+                  newUser = signUser(newUser);
+                  return done(null, newUser);
                 });
               }
-              return done(null, user); // user found, return that user
-            } else {
-              // if there is no user, create them
-              var newUser = new models.User();
-              newUser.facebook.id = profile.id;
-              newUser.facebook.token = token;
-              newUser.name.last = profile._json.last_name;
-              newUser.name.first = profile._json.first_name;
-              newUser.email = profile._json.email;
-              newUser.img_url = profile._json.picture.data.url;
-              newUser.username = '';
-              newUser.save((err) => {
-                if (err) {
-                  return done(err);
-                }
-                return done(null, newUser);
-              });
-            }
-          });
+            })
+            .populate({
+              path: 'role',
+              select: 'title'
+            });
         } else {
           // user already exists and is logged in, we have to link accounts
           var user = req.user; // pull the user out of the session
@@ -85,24 +86,19 @@ var FacebookStrategy = require('passport-facebook').Strategy,
               if (err) {
                 return done(err);
               }
-              return done(null, user);
-            });
-          } else {
-            var newUser = new models.User();
-            newUser.facebook.id = profile.id;
-            newUser.facebook.token = token;
-            newUser.name.last = profile._json.last_name;
-            newUser.name.first = profile._json.first_name;
-            newUser.email = profile._json.email;
-            newUser.img_url = profile._json.picture.data.url;
-            newUser.username = '';
-            newUser.save((err) => {
-              if (err) {
-                return done(err);
-              }
-              return done(null, newUser);
             });
           }
+          models.User.findOne({
+              'facebook.id': profile.id
+            }, (err, user) => {
+              if (err) return done(err);
+              user = signUser(user);
+              return done(null, user);
+            })
+            .populate({
+              path: 'role',
+              select: 'title'
+            });
         }
       });
     }));
@@ -119,48 +115,38 @@ var FacebookStrategy = require('passport-facebook').Strategy,
       process.nextTick(() => {
         // check if the user is already logged in
         if (!req.user) {
-          console.log(profile.id);
           models.User.findOne({
-            'google.id': profile.id
-          }, (err, user) => {
-            if (err) {
-              return done(err);
-            }
-            if (user) {
-              // if there is a user id already but no token
-              // (user was linked at one point and then removed)
-              if (!user.google.token) {
-                user.google.token = token;
-                user.google.name = profile.displayName;
+              'google.id': profile.id
+            }, (err, user) => {
+              if (err) {
+                return done(err);
+              }
+              if (user) {
+                user = signUser(user);
+                return done(null, user);
+              } else {
+                var newUser = new models.User(),
+                  fullname = profile._json.name.givenName.split(' ');
+                newUser.google.id = profile.id;
+                newUser.google.token = token;
+                newUser.name.last = fullname[0];
+                newUser.name.first = fullname[1];
                 // pull the first email
-                user.google.email = (profile.emails[0].value || '');
-                user.save((err) => {
+                newUser.email = profile._json.emails[0].value;
+                newUser.img_url = profile._json.image.url;
+                newUser.save((err) => {
                   if (err) {
                     return done(err);
                   }
-                  return done(null, user);
+                  newUser = signUser(newUser);
+                  return done(null, newUser);
                 });
               }
-              return done(null, user);
-            } else {
-              var newUser = new models.User(),
-                fullname = profile._json.name.givenName.split(' ');
-              newUser.google.id = profile.id;
-              newUser.google.token = token;
-              newUser.name.last = fullname[0];
-              newUser.name.first = fullname[1];
-              // pull the first email
-              newUser.email = (profile._json.emails[0].value || '');
-              newUser.img_url = profile._json.image.url;
-              newUser.username = '';
-              newUser.save((err) => {
-                if (err) {
-                  return done(err);
-                }
-                return done(null, newUser);
-              });
-            }
-          });
+            })
+            .populate({
+              path: 'role',
+              select: 'title'
+            });
         } else {
           // user already exists and is logged in, we have to link accounts
           var user = req.user, // pull the user out of the session
@@ -175,26 +161,19 @@ var FacebookStrategy = require('passport-facebook').Strategy,
               if (err) {
                 return done(err);
               }
-              return done(null, user);
-            });
-          } else {
-            var newUser = new models.User(),
-              _fullname = profile._json.name.givenName.split(' ');
-            newUser.google.id = profile.id;
-            newUser.google.token = token;
-            newUser.name.last = _fullname[0];
-            newUser.name.first = _fullname[1];
-            // pull the first email
-            newUser.email = (profile._json.emails[0].value || '');
-            newUser.img_url = profile._json.image.url;
-            newUser.username = '';
-            newUser.save((err) => {
-              if (err) {
-                return done(err);
-              }
-              return done(null, newUser);
             });
           }
+          models.User.findOne({
+              'google.id': profile.id
+            }, (err, user) => {
+              if (err) return done(err);
+              user = signUser(user);
+              return done(null, user);
+            })
+            .populate({
+              path: 'role',
+              select: 'title'
+            });
         }
       });
     }));
