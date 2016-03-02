5e368b38aa6a4a4c7eb5f3ce8e4554220b59a084
Lawrence Mcbheee <lawrence.olaiya@andela.com>
2016-03-02 15:51:37 +0100

    Remove redundant lines in db-connect function

--- app/config/db-connect.js
+++ app/config/db-connect.js
@@ -6,24 +6,6 @@ var db_connect = () => {
   Mongoose.connection.on('connected', () => {
     console.log('Mongoose has connected to the database specified.');
   });
-
-  // If the connection throws an error
-  Mongoose.connection.on('error', err => {
-    console.log('Mongoose default connection error: ' + err);
-  });
-
-  // When the connection is disconnected
-  Mongoose.connection.on('disconnected', () => {
-    console.log('Mongoose default connection disconnected');
-  });
-
-  // If the Node process ends, close the Mongoose connection
-  process.on('SIGINT', () => {
-    Mongoose.connection.close(() => {
-      console.log('Mongoose disconnected on application exit');
-      process.exit(0);
-    });
-  });
 };
 
 module.exports = db_connect;
